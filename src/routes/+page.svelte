28946a9da58be4e67db0b422dc2aaf6a2dc35fb9
Courtney Fay <fay.courtney@gmail.com>
2023-09-14 08:29:47 -0400

    Adding filtering to views

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let selectedId = 1;
+    const views = [
+		{ id: 1, name: 'One Moon' },
+		{ id: 2, name: 'A Few Moons' },
+		{ id: 3, name: 'Half Moon' },
+		{ id: 4, name: 'New Moon' },
+		{ id: 5, name: 'Full Moon' },
+		{ id: 6, name: 'Astrological Sign' },
+		{ id: 7, name: 'Season' },
+		{ id: 8, name: 'Year' },
+	];
+</script>
+
+
+
+<h1>Moon Calendar</h1>
+
+<div>
+    <p>Filter buttons:</p>
+    <select bind:value={selectedId}>
+		{#each views as view}
+			<option value={view.id}>
+				{view.name}
+			</option>
+		{/each}
+	</select>
+</div>
+
+<p>Current View: { selectedId }</p>
