ba7713e734293cb4098968854b23a195876085f0
Courtney Fay <fay.courtney@gmail.com>
2023-09-14 09:42:09 -0400

    Adding different logic for each select option

---
+++ src/lib/components/MoonCalendar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export let id: number;
+    let date: Date = new Date();
+</script>
+
+<p>Current View: { id }</p>
+
+{#if id === 1}
+    <p>Show one moon</p>
+    <p>Date: { date }</p>
+{:else if id === 2}
+    <p>Show yesterday, today and tomorrow moons</p>
+{:else if id === 3}
+    <p>Show from either the closest full moon to new moon, or the closest new moon to full moon</p>
+{:else if id === 4}
+    <p>Show from most recent new moon to next new moon</p>
+{:else if id === 5}
+    <p>Show from most recent full moon to next full moon</p>
+{:else if id === 6}
+    <p>Show from most recent astrological sign change to next astrological sign change</p>
+{:else if id === 7}
+    <p>Show from most recent season beginning (equinox or solstice) to next season equinox/solstice</p>
+{:else}
+    <p>what did you select?</p>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
     import FilterViews from '$lib/components/FilterViews.svelte'
+    import MoonCalendar from '$lib/components/MoonCalendar.svelte'
     let id = 1;
 </script>
-
-
 
 <h1>Moon Calendar</h1>
 
 <FilterViews bind:selectedId={ id } />
-
-<p>Current View: { id }</p>
+<MoonCalendar id={ id } />
