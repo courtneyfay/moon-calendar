ce4151f93501914d46c00d324a7ec4a47d02c6e6
Courtney Fay <courtneyfay@users.noreply.github.com>
2023-09-14 09:22:35 -0400

    Moving select into its own component

---
+++ src/lib/components/FilterViews.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    export let selectedId: number;
+    const views = [
+		{ id: 1, name: 'One Moon' },
+		{ id: 2, name: 'A Few Moons' },
+		{ id: 3, name: 'Half Moon' },
+		{ id: 4, name: 'New Moon' },
+		{ id: 5, name: 'Full Moon' },
+		{ id: 6, name: 'Astrological Sign' },
+		{ id: 7, name: 'Season' },
+		{ id: 8, name: 'Year' },
+	];
+</script>
+
+<div>
+    <p>Filter buttons:</p>
+    <select bind:value={selectedId}>
+		{#each views as view}
+			<option value={view.id}>
+				{view.name}
+			</option>
+		{/each}
+	</select>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,12 @@
 <script lang="ts">
-    let selectedId = 1;
-    const views = [
-		{ id: 1, name: 'One Moon' },
-		{ id: 2, name: 'A Few Moons' },
-		{ id: 3, name: 'Half Moon' },
-		{ id: 4, name: 'New Moon' },
-		{ id: 5, name: 'Full Moon' },
-		{ id: 6, name: 'Astrological Sign' },
-		{ id: 7, name: 'Season' },
-		{ id: 8, name: 'Year' },
-	];
+    import FilterViews from '$lib/components/FilterViews.svelte'
+    let id = 1;
 </script>
 
 
 
 <h1>Moon Calendar</h1>
 
-<div>
-    <p>Filter buttons:</p>
-    <select bind:value={selectedId}>
-		{#each views as view}
-			<option value={view.id}>
-				{view.name}
-			</option>
-		{/each}
-	</select>
-</div>
+<FilterViews bind:selectedId={ id } />
 
-
-<p>Current View: { selectedId }</p>
+<p>Current View: { id }</p>
