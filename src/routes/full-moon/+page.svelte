983f2a53993800a11a4af85be11639feafca56c3
Courtney Fay <courtneyfay@users.noreply.github.com>
2023-11-22 14:16:06 -0500

    loading screen shows up for all pages now

--- src/routes/full-moon/+page.svelte
+++ src/routes/full-moon/+page.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
+    import Loader, { wait } from '$lib/components/Loader.svelte'
     import {
         fullMoon,
         formatDate,
     } from '$lib/data/dates'    
 </script>
 
-<!-- TODO: create full moon/new moon/astrology sign template -->
-<p>Previous full moon - { formatDate(fullMoon.previous) }</p>
-{#each fullMoon.interval as fullMoonDate}
-    <p>{formatDate(fullMoonDate)}</p>
-{/each}
-<p>Next full moon - { formatDate(fullMoon.next) }</p>
+{#await wait()}
+    <Loader />
+{:then}
+    <!-- TODO: create full moon/new moon/astrology sign template -->
+    <p>Previous full moon - { formatDate(fullMoon.previous) }</p>
+    {#each fullMoon.interval as fullMoonDate}
+        <p>{formatDate(fullMoonDate)}</p>
+    {/each}
+    <p>Next full moon - { formatDate(fullMoon.next) }</p>
+{/await}
 
 <style>
 </style>
--- src/routes/season/+page.svelte
+++ src/routes/season/+page.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
+    import Loader, { wait } from '$lib/components/Loader.svelte'
     import {
         seasons,
         formatDate,
     } from '$lib/data/dates'    
 </script>
 
-<p>Previous season start date - { formatDate(seasons.previous) }</p>
-{#each seasons.interval as seasonDate}
-    <p>{formatDate(seasonDate)}</p>
-{/each}
-<p>Next season start date - { formatDate(seasons.next) }</p>
+{#await wait()}
+    <Loader />
+{:then}
+    <p>Previous season start date - { formatDate(seasons.previous) }</p>
+    {#each seasons.interval as seasonDate}
+        <p>{formatDate(seasonDate)}</p>
+    {/each}
+    <p>Next season start date - { formatDate(seasons.next) }</p>
+{/await}
 
 <style>
 </style>
