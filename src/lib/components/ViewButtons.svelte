2979ec30f64b18461b9cbc865e31cea1f802f02b
Courtney Fay <courtneyfay@users.noreply.github.com>
2023-12-11 13:45:48 -0500

    adding gong sound to each button click

--- src/lib/components/ViewButtons.svelte
+++ src/lib/components/ViewButtons.svelte
@@ -1,18 +1,44 @@
 <script lang="ts">
+	import { goto } from '$app/navigation'
+
 	import { page } from '$app/stores'
-    import { views } from '$lib/data/views'
+    import { views, type TView } from '$lib/data/views'
+
+	// plays gong sound when button is clicked
+	let src = '/gong.mp3'
+	let time = 0
+	let paused = true
+
+	const handleClick = (view: TView) => {
+		paused = !paused
+		goto(`/${ view.slug }`)
+	}
 </script>
 
 <nav>
+	<audio
+		{src}
+		bind:currentTime={time}
+		bind:paused
+		preload="metadata"
+		on:ended={() => {
+			time = 0
+			paused = true
+		}}
+	/>
+
 	{#each views as view}
-		<a href={ `/${ view.slug }` } class:active={ $page.url.pathname.includes(view.slug) }>
+		<button
+			on:click={() => handleClick(view)}
+			class:active={ $page.url.pathname.includes(view.slug) }
+		>
 			{ view.name }
-		</a>
+		</button>
 	{/each}
 </nav>
 
 <style>
-	a {
+	button {
 		/* font */
 		font-family: 'Merriweather', serif;
 		color: #685044;
@@ -26,12 +52,13 @@
 		margin-right: 5px;
 	}
 
-	a:hover {
+	button:hover {
 		color: white;
 		background-color: #801A86;
+		cursor: pointer;
 	}
 
-	a.active {
+	button.active {
 		color: white;
 		background-color: #801A86;
 	}
