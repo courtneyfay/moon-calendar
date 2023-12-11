5a96f5e6efee0803e12828e2849d9416849c625d
Courtney Fay <courtneyfay@users.noreply.github.com>
2023-12-11 15:53:30 -0500

    adding interactivity to object

--- src/lib/components/3D/OneMoonScene.svelte
+++ src/lib/components/3D/OneMoonScene.svelte
@@ -1,5 +1,11 @@
 <script>
   import { T } from '@threlte/core'
+  import { interactivity } from '@threlte/extras'
+  import { spring } from 'svelte/motion'
+
+  // makes interactivity possible
+  interactivity()
+  const scale = spring(1)
 </script>
   
 <!-- Adding perspective camera -->
@@ -12,7 +18,12 @@
 />
 
 <!-- Long 2D pink rectangle -->
-<T.Mesh position.y={1}>
+<T.Mesh
+  position.y={1}
+  scale={$scale}
+  on:pointerenter={() => scale.set(1.5)}
+  on:pointerleave={() => scale.set(1)}
+>
   <T.BoxGeometry args={[1, 2, 1]} />
   <T.MeshBasicMaterial color="hotpink" />
 </T.Mesh>
