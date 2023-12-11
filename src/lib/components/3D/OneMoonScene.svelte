eb95cfff0b8b5aea7f67db7f821583b1a0e25854
Courtney Fay <courtneyfay@users.noreply.github.com>
2023-12-11 15:11:04 -0500

    adding perspective camera

--- src/lib/components/3D/OneMoonScene.svelte
+++ src/lib/components/3D/OneMoonScene.svelte
@@ -1,8 +1,18 @@
 <script>
-    import { T } from '@threlte/core'
-  </script>
+  import { T } from '@threlte/core'
+</script>
   
-  <T.Mesh>
-    <T.BoxGeometry />
-    <T.MeshBasicMaterial />
-  </T.Mesh>
+<!-- Adding perspective camera -->
+<T.PerspectiveCamera
+  makeDefault
+  position={[10, 10, 10]}
+  on:create={({ ref }) => {
+    ref.lookAt(0, 1, 0)
+  }}
+/>
+
+<!-- Long 2D pink rectangle -->
+<T.Mesh position.y={1}>
+  <T.BoxGeometry args={[1, 2, 1]} />
+  <T.MeshBasicMaterial color="hotpink" />
+</T.Mesh>
