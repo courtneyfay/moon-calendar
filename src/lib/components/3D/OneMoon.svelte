f7e5d0dbb137bc5311d810102a91539d27af14fc
Courtney Fay <courtneyfay@users.noreply.github.com>
2023-12-15 11:31:04 -0500

    adding colors for activities

--- src/lib/components/3D/OneMoon.svelte
+++ src/lib/components/3D/OneMoon.svelte
@@ -10,7 +10,6 @@
   })
 
   const sphereSize = 0.5
-  const sphereColor = '#ADB993'
 
   const generateSphereLabel = (labelText: string) => {
     const text = document.createElement('div')
@@ -21,6 +20,7 @@
   type TSphere = {
     position: [number, number, number],
     eventTitle: HTMLDivElement,
+    sphereColor: string,
   }
 
   // hardcoded ring of spheres
@@ -29,106 +29,130 @@
     // quadrant one
     {
         position: [0, 3, 0],
-        eventTitle: generateSphereLabel('11 am'),
+        eventTitle: generateSphereLabel('11 am - Homework'),
+        sphereColor: '#ADB993',
     },
     {
         position: [0.8, 2.9, 0],
-        eventTitle: generateSphereLabel('12 pm'),
+        eventTitle: generateSphereLabel('12 pm - Workshop'),
+        sphereColor: '#ADB993',
     },
     {
         position: [1.5, 2.6, 0],
-        eventTitle: generateSphereLabel('1 pm'),
+        eventTitle: generateSphereLabel('1 pm - Workshop'),
+        sphereColor: '#ADB993',
     },
     {
         position: [2.12, 2.12, 0],
-        eventTitle: generateSphereLabel('2 pm'),
+        eventTitle: generateSphereLabel('2 pm - Homework'),
+        sphereColor: '#ADB993',
     },
     {
         position: [2.6, 1.5, 0],
-        eventTitle: generateSphereLabel('3 pm'),
+        eventTitle: generateSphereLabel('3 pm - Homework'),
+        sphereColor: '#ADB993',
     },
     {
         position: [2.9, 0.8, 0],
-        eventTitle: generateSphereLabel('4 pm'),
+        eventTitle: generateSphereLabel('4 pm - Homework'),
+        sphereColor: '#ADB993',
     },
     
 
     // quadrant two
     {
         position: [3, 0, 0],
-        eventTitle: generateSphereLabel('5 pm'),
+        eventTitle: generateSphereLabel('5 pm - Holiday Party'),
+        sphereColor: '#ADB993',
     },
     {
         position: [2.9, -0.8, 0],
-        eventTitle: generateSphereLabel('6 pm'),
+        eventTitle: generateSphereLabel('6 pm - Holiday Party'),
+        sphereColor: '#ADB993',
     },
     {
         position: [2.6, -1.5, 0],
-        eventTitle: generateSphereLabel('7 pm'),
+        eventTitle: generateSphereLabel('7 pm - Whim Time'),
+        sphereColor: '#ADB993',
     },
     {
         position: [2.12, -2.12, 0],
-        eventTitle: generateSphereLabel('8 pm'),
+        eventTitle: generateSphereLabel('8 pm - Whim Time'),
+        sphereColor: '#ADB993',
     },
     {
         position: [1.5, -2.6, 0],
         eventTitle: generateSphereLabel('9 pm'),
+        sphereColor: 'white',
     },
     {
         position: [0.8, -2.9, 0],
         eventTitle: generateSphereLabel('10 pm'),
+        sphereColor: 'white',
     },
 
     // quadrant three
     {
         position: [0, -3, 0],
         eventTitle: generateSphereLabel('11 pm'),
+        sphereColor: 'white',
     },
     {
         position: [-0.8, -2.9, 0],
         eventTitle: generateSphereLabel('12 am'),
+        sphereColor: 'white',
     },
     {
         position: [-1.5, -2.6, 0],
         eventTitle: generateSphereLabel('1 am'),
+        sphereColor: 'white',
     },
     {
         position: [-2.12, -2.12, 0],
         eventTitle: generateSphereLabel('2 am'),
+        sphereColor: 'white',
     },
     {
         position: [-2.6, -1.5, 0],
         eventTitle: generateSphereLabel('3 am'),
+        sphereColor: 'white',
     },
     {
         position: [-2.9, -0.8, 0],
         eventTitle: generateSphereLabel('4 am'),
+        sphereColor: 'white',
     },
 
     // quadrant four
     {
         position: [-3, 0, 0],
         eventTitle: generateSphereLabel('5 am'),
+        sphereColor: 'white',
     },
     {
         position: [-2.9, 0.8, 0],
-        eventTitle: generateSphereLabel('6 am'),
+        eventTitle: generateSphereLabel('6 am - Recycling out'),
+        sphereColor: '#ADB993',
     },
     {
         position: [-2.6, 1.5, 0],
         eventTitle: generateSphereLabel('7 am'),
+        sphereColor: 'white',
     },
     {
         position: [-2.12, 2.12, 0],
         eventTitle: generateSphereLabel('8 am'),
+        sphereColor: 'white',
     },
     {
         position: [-1.5, 2.6, 0],
         eventTitle: generateSphereLabel('9 am'),
+        sphereColor: 'white',
     },
     {
         position: [-0.8, 2.9, 0],
-        eventTitle: generateSphereLabel('10 am'),
+        eventTitle: generateSphereLabel('10 am - Homework'),
+        sphereColor: '#ADB993',
     },
   ]
 
@@ -220,7 +244,7 @@
     >
       <T.Mesh>
         <T.SphereGeometry args={[sphereSize]} />
-        <T.MeshStandardMaterial color={sphereColor} />
+        <T.MeshStandardMaterial color={sphere.sphereColor} />
       </T.Mesh>
     </T.CSS2DObject>
   {/each}
